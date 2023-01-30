b05ceb91ea222f6fc15946191f4b2b0e7d534e9f
Mateus Azevedo <mateusazevedoaguiar@gmail.com>
2023-01-30 17:32:12 -0300

    ai ai esse react

---
+++ src/components/CardFilme/index.tsx
@@ -0,0 +1,15 @@
+
+
+
+export const CardFilme = ()=>{
+  return(
+    <>
+      <div className="card-container" style={{backgroundImage: `url("https://m.media-amazon.com/images/M/ MV5BMzdjNjI5MmYtODhiNS00NTcyLWEzZmUtYzVmODM5YzExNDE3XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_SX300.jpg")`}}>
+              <div className='card-rating'>7.2 <span> <AiFillStar/> </span></div>
+              <div className='card-title'>The Menu</div>
+              <div className='card-genero'>Terror</div>
+              
+      </div> 
+    </>
+  )
+}
--- src/components/ListaDeCard/index.tsx
+++ src/components/ListaDeCard/index.tsx
@@ -1,24 +1,18 @@
 
 import './listadecard.css'
-
+import { AiFillStar } from 'react-icons/ai';
+import { CardFilme } from '../CardFilme';
+import { objMovies } from '../../helpers/getMovies';
 export const ListaDeCard = ()=>{
+
+  let arr = [1,3,5,7,9,10]
+
   return(
     <>
       <div className="list-container">
-            <div className="card-container">
-              <img src="" alt="" />
-            </div> 
-            <div className="card-container">
-            </div> 
-            <div className="card-container">
-            </div> 
-            <div className="card-container">
-            </div> 
-            <div className="card-container">
-            </div> 
-            <div className="card-container">
-            </div>
-            
+        {arr.map((i)=>{
+          
+        })}
       </div>
     </>
   )
