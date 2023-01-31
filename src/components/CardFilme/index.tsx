5e0406b75179748715fa943b540efa24de36877b
Lucas Alves <zombnfl@gmail.com>
2023-01-31 17:39:56 -0300

    derrota do dia 2

--- src/components/CardFilme/index.tsx
+++ src/components/CardFilme/index.tsx
@@ -1,13 +1,9 @@
 import { AiFillStar } from 'react-icons/ai';
-import { BrowserRouter, Link, Route, Routes} from "react-router-dom"
+
+
 export const CardFilme = ({obj}:any)=>{
   return(
-    <>
-      <BrowserRouter>
-        <Routes>
-        <Route path={`movies/${obj.id}`} />
-        </Routes>
-        <Link to={"movies/" + obj.id}/>
+    <>  
         <div className="card-container" style={{backgroundImage: `url(${obj.imgUrl})`}}>
         </div> 
         <div className='mascara'>
@@ -17,7 +13,6 @@ export const CardFilme = ({obj}:any)=>{
             <div className='card-genero'>{obj.genre}</div>
           </div>
         </div>
-      </BrowserRouter>
     </>
   )
 }
