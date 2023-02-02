5bbf4fbccb99cacc632b072f77b57b810a14ed24
Mateus Azevedo <mateusazevedoaguiar@gmail.com>
2023-02-02 15:41:26 -0300

    teste

--- src/pages/Filme/index.tsx
+++ src/pages/Filme/index.tsx
@@ -40,6 +40,12 @@ export const Filme = ()=>{
         <Comentario/>
         <Comentario/>
         <Comentario/>
+        <Comentario/>
+        <Comentario/>
+        <Comentario/>
+        <Comentario/>
+        <Comentario/>
+        <Comentario/>
       </div>
     </>
   )
