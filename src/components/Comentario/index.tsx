38af4ca38c6e4c6a931d27b6ce3f03c66db614e7
Lucas Alves <zombnfl@gmail.com>
2023-02-02 17:46:15 -0300

    Lógica de likes, dislikes e impressions

--- src/components/Comentario/index.tsx
+++ src/components/Comentario/index.tsx
@@ -1,6 +1,6 @@
 import './comentario.css'
 import {useState} from 'react'
-//like
+import Critic from '../../helpers/Critic';
 import { AiOutlineLike } from 'react-icons/ai';
 import { AiFillLike } from 'react-icons/ai';
 //deslike
@@ -11,6 +11,17 @@ export const Comentario = ({obj} : any, key : any)=>{
   // id content respostas author numero de likes e deslikes
   const [isLike,setIsLike] = useState(false)
   const [isDeslike,setIsDeslike] = useState(false)
+  const [likes, setLikes] = useState(0);
+
+  function incrementaLike()
+  {
+    setLikes(
+      obj.likes++
+    )
+    console.log(obj);
+    
+  }
+  
   function mostraLike(){
     if(isLike && isDeslike == false){
       return <AiFillLike className='icon'/>
@@ -37,7 +48,10 @@ export const Comentario = ({obj} : any, key : any)=>{
         <h4>Monkey D. Luffy</h4>
         <p>{obj.content}</p>
         </div>
-        <div className='like' onClick={()=>setIsLike(!isLike)}>
+        <div className='like' onClick={()=>{
+              setIsLike(!isLike)
+              incrementaLike()
+              }}>
           {mostraLike()}
         </div>
         <div className='like' onClick={()=>setIsDeslike(!isDeslike)}>
